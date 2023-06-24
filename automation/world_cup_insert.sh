a83f5e99de6dc3946054c60e38111288e1a48fbf
maulanazn <starknntnk99@gmail.com>
2023-06-24 08:49:43 +0700

    query based on expected output is clear

--- automation/world_cup_insert.sh
+++ automation/world_cup_insert.sh
@@ -9,6 +9,8 @@ fi
 
 # Do not change code above this line. Use the PSQL variable above to query your database.
 echo $($PSQL "TRUNCATE games, teams")
+echo $($PSQL "alter sequence games_game_id_seq restart with 1")
+echo $($PSQL "alter sequence teams_team_id_seq restart with 1")
 cat csv/games.csv | while IFS="," read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
 do
   if [[ $YEAR != "year" ]]
@@ -16,29 +18,17 @@ do
     WINNER_ID=$($PSQL "SELECT name FROM teams WHERE name='$WINNER'")
     if [[ -z $WINNER_ID ]]
     then
-      INSERT_WINNNER_RESULT=$($PSQL "INSERT INTO teams(name) VALUES('$WINNER')")
-      if [[ $INSERT_WINNNER_RESULT == "INSERT 0 1" ]]
-      then
-        echo Inserted into teams, $WINNER
-      fi
+      $($PSQL "INSERT INTO teams(name) VALUES('$WINNER')")
     fi
     
     OPPONENT_ID=$($PSQL "SELECT name FROM teams WHERE name='$OPPONENT'")
     if [[ -z $OPPONENT_ID ]]
     then
-      INSERT_OPPONENT_RESULT=$($PSQL "INSERT INTO teams(name) VALUES('$OPPONENT')")
-      if [[ $INSERT_OPPONENT_RESULT == "INSERT 0 1" ]]
-      then
-        echo Inserted into teams, $OPPONENT
-      fi
+      $($PSQL "INSERT INTO teams(name) VALUES('$OPPONENT')")
     fi
 
     WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
     OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")
-    INSERT_GAMES_RESULT=$($PSQL "INSERT INTO games(year, round, winner_id, opponent_id, winner_goals, opponent_goals) VALUES($YEAR, '$ROUND', '$WINNER_ID', '$OPPONENT_ID', $WINNER_GOALS, $OPPONENT_GOALS)")
-    if [[ $INSERT_GAMES_RESULT == "INSERT 0 1" ]]
-    then
-      echo Inserted into games, $YEAR $ROUND $WINNER_ID $OPPONENT_ID $WINNER_GOALS $OPPONENT_GOALS
-    fi
+    $($PSQL "INSERT INTO games(year, round, winner_id, opponent_id, winner_goals, opponent_goals) VALUES($YEAR, '$ROUND', $WINNER_ID, $OPPONENT_ID, $WINNER_GOALS, $OPPONENT_GOALS)")
   fi
 done
